52c8c972afd8bd5c7c90530412b3b33a1fde6417
unknown <jwan>
2019-12-17 15:40:31 -0600

    some update

--- js/index31.js
+++ js/index31.js
@@ -1,5 +1,8 @@
-const firsSide = 10;
+let firsSide = 10;
 const SecondSide = 20;
 const ThirdSide = 5;
 const perimiterOfTriangle = firsSide+SecondSide+ThirdSide ;
 console.log(`perimiter of a triangle has lengths ${firsSide},${SecondSide},${ThirdSide} is',perimiterOfTriangle`);
+console.log(firsSide++);
+console.log(++firsSide);
+console.log(7 & 4);
